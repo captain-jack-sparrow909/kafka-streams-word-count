8ee58ef470cc46ce0a463b79adfef0e2af928eb9
Jabir <15pwele4721@uetpeshawar.edu.pk>
2025-08-18 20:13:54 +0400

    Kafka Streams Starter app for word count

---
+++ src/main/java/org/khanjabir909/kafkastreams/StreamsStarterApp.java
@@ -0,0 +1,7 @@
+package org.khanjabir909.kafkastreams;
+
+public class StreamsStarterApp {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome to Kafka Streams Starter App!");
+    }
+}
