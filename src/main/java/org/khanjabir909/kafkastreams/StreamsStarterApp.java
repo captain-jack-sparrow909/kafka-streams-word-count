f911ad5edbc51f759ab4d0375a4e1fcc805d4b25
Jabir <15pwele4721@uetpeshawar.edu.pk>
2025-08-19 20:21:25 +0400

    added properties

--- src/main/java/org/khanjabir909/kafkastreams/StreamsStarterApp.java
+++ src/main/java/org/khanjabir909/kafkastreams/StreamsStarterApp.java
@@ -1,7 +1,18 @@
 package org.khanjabir909.kafkastreams;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsConfig;
+
+import java.util.Properties;
+
 public class StreamsStarterApp {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome to Kafka Streams Starter App!");
+        Properties properties = new Properties();
+        properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-starter-app");
+        properties.setProperty(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
     }
 }
